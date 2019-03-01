f52c3bb94614c66e82a51db1eedae53bfe3fc136
Quit Phuong Bui <quitpb@stud.ntnu.no>
2019-03-01 12:30:09 +0100

    distinguish users

--- account/urls.py
+++ account/urls.py
@@ -6,7 +6,8 @@ from django.contrib.auth.models import User
 
 
 urlpatterns = [
-    path('<username>', views.account_home, name='account_home'),
+#     path('<username>', views.account_home, name='account_home'),
+     path('<username>', views.get_user_profile, name='get_user_profile')
 #    url(r'^/(?P<username>\w+)/$', views.account, name='account'),
 #    url(r'^/(?P<username>\w+)/$', 'account_home', name='account_home'),
 ]
--- account/views.py
+++ account/views.py
@@ -5,11 +5,25 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 
-
 @login_required
-def account_home(request, username):
-    print(request.user.username)
-    return render(request, 'account.html')
+def get_user_profile(request, username):
+    my_user = request.user.username
+    if my_user == username:
+        print(request.user.username)
+        return render(request, 'account.html')
+    else:
+        user = User.objects.get(username=username)
+        user_first_name = user.first_name
+        user_last_name = user.last_name
+        user_email = user.email
+        user_username = user.username
+        print(user.username)
+        return render(request, 'user_profile.html', {
+            "user_username": user_username,
+            "user_first_name": user_first_name,
+            "user_last_name": user_last_name,
+            "user_email": user_email,
+        })
 
 
 
