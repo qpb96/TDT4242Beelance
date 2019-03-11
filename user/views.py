a66e5d817b6092b79bbb394a4b8cbd7f7f06bd98
Quit Phuong Bui <quitpb@stud.ntnu.no>
2019-03-11 13:35:13 +0100

    FR29 Add logic

--- user/views.py
+++ user/views.py
@@ -11,7 +11,7 @@ from django.utils import timezone
 
 
 from .forms import SignUpForm, EditProfileForm, UserForm, PostReviewForm
-from projects.models import Project
+from projects.models import Project, Task
 from .models import Review
 
 def index(request):
@@ -45,8 +45,7 @@ def view_user_profile(request, username):
         return render(request, 'user/myaccount.html')
     else:
         user = get_object_or_404(User, username=username)
-        print(user.username)
-        review_available = isAnyProjectFinished(request, user.username)
+        review_available = review_possible(request, user.username)
         return render(request, 'user/userprofile.html', {
             "user_username": user.username,
             "user_first_name": user.first_name,
@@ -105,7 +104,6 @@ def edit_user_profile(request, user_id):
 
 def createReview(request, username):
     profile = User.objects.get(username=username)
-    participantInProject(request, username)
     form = PostReviewForm(request.POST)
     if request.method == 'POST':
         form = PostReviewForm(request.POST)
@@ -122,34 +120,88 @@ def createReview(request, username):
 
     return render(request, 'user/add_review.html', {'form': form, })
 
-#Check if there exist any finished projects where request.user is a participant
-def isAnyProjectFinished(request, projectOwner):
-    if participantInProject(request, projectOwner):
-        projects = ProjectByUser(projectOwner)
+
+def review_possible(request, project_owner):
+    if is_any_project_finished(request, project_owner) or task_delivered_or_declined(request, project_owner):
+        return True
+    return False
+
+
+# Check status of a task. Return True if task.status = delivered or declined
+def task_delivered_or_declined(request, project_owner):
+    tasks = tasks_by_user(project_owner)
+    for task in tasks:
+        print("""""""")
+        print(task)
+        if check_user_in_task(request, task):
+            print("user in task")
+            if task.status == 'pa' or task.status == 'dd':
+                return True
+    print("RIIIIIP")
+    return False
+
+
+# Check if there exist any finished projects where request.user is a participant
+def is_any_project_finished(request, project_owner):
+    if participant_in_project(request, project_owner):
+        projects = project_by_user(project_owner)
         for project in projects:
             if project.status == 'f':
                 return True
     return False
 
 
-#See if request.user is a participant in any project of <username>
-def participantInProject(request, projectOwner):
-    projects = ProjectByUser(projectOwner)
+# See if request.user is a participant in any project of <username>
+def participant_in_project(request, project_owner):
+    projects = project_by_user(project_owner)
     for project in projects:
-        myParticipant = project.participants.all()
-        for participant in myParticipant:
-            if(request.user.profile == participant):
+        all_participants = project.participants.all()
+        for participant in all_participants:
+            if request.user.profile == participant:
                 return True
     return False
 
-#Search for all projects made by a user
-def ProjectByUser(username):
+
+# Search for all projects made by a user
+def project_by_user(username):
     list_of_projects = Project.objects.all()
     projects_by_user = []
     for project in list_of_projects:
-        if(project.user.user.username == username):
+        if project.user.user.username == username:
             projects_by_user.append(project)
     return projects_by_user
 
 
+def tasks_of_project(project_name):
+    project = Project.objects.get(title=project_name)
+    tasks_of_project = []
+    project_tasks = project.tasks.all()
+    print(project_tasks)
+    for task in project_tasks:
+        tasks_of_project.append(task)
+    return tasks_of_project
 
+
+def tasks_by_user(username):
+    projects = project_by_user(username)
+    list_tasks = []
+    for project in projects:
+        tasks = tasks_of_project(project.title)
+        for task in tasks:
+            list_tasks.append(task)
+    return tasks
+
+
+def check_user_in_task(request, taskobject):
+    all_tasks = Task.objects.all()
+    task1 = None
+    participants = None
+    for t in all_tasks:
+        if taskobject == t:
+            task1 = t;
+            participants = task1.read.all()
+    print(participants)
+    for profile in participants:
+        if request.user.profile == profile:
+            return True
+    return False
